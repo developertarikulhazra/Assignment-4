2f26c22d6cfe7c4144eced3e4c77f7ff1651ff0c
developertarikulhazra <tariqulhazra1614@gmail.com>
2023-02-07 21:10:14 +0600

    done

--- index.js
+++ index.js
@@ -1,64 +1,114 @@
 // First Problem
 
-function x(data) {
-  let a = data * 3;
-  let b = a + 10;
-  let c = b / 2;
-  let d = c - 5;
-
-  return d;
+function mindGame(number) {
+  if (isNaN(number)) {
+    return "Please Enter a valid number";
+  }
+  let numberOne = number * 3;
+  let numberTwo = numberOne + 10;
+  let numberThree = numberTwo / 2;
+  let total = numberThree - 5;
+  return total;
 }
 
-console.log(x(33));
+const result = mindGame("33");
+
+console.log(result);
+
+/* The function first multiplies the input Number by 3, Then, it adds 10, it divides numberTwo by 2, Finally, it subtracts 5,the code logs the value of result to the console using console.log.*/
 
 // Second Problem
 
-function z(data) {
-  let length = data.length;
+function evenOdd(text) {
+  if (!isNaN(text)) {
+    return "Please Enter a valid number";
+  }
+  let length = text.length;
   if (length % 2 === 0) {
     return "even";
   } else {
     return "odd";
   }
 }
-console.log(z("Batch7"));
+const characterNumber = evenOdd("chatgpt");
+
+console.log(characterNumber);
+
+/* Function uses an if/else statement to check if the length is even or odd. If the length is evenly divisible by 2, the function returns the string "even". If not, it returns the string "odd".*/
 
 // Third Problem
 
-function y(data) {
-  if (data < 7) {
-    return data - 7;
+function isLGSeven(number) {
+  if (isNaN(number)) {
+    return "Please Enter a valid number";
+  }
+  if (number < 7) {
+    const sumNumberOne = number - 7;
+    return sumNumberOne;
   } else {
-    return data * 2;
+    const sumNumberTwo = number * 2;
+    return sumNumberTwo;
   }
 }
+const sum = isLGSeven("15");
 
-console.log(y(15));
+console.log(sum);
+
+/* If number is less than 7, the function calculates sumNumberOne as number - 7 and returns its value.If number is greater than or equal to 7, the function calculates sumNumberTwo as number * 2 and returns its value.*/
+
 // Fourth Problem
+
 function findingBadData(array) {
-  let count = 0;
+  let result = 0;
+
   for (let i = 0; i < array.length; i++) {
-    if (array[i] < 0) {
-      count++;
+    if (isNaN(array[i])) {
+      return "Please Enter a valid number";
+    } else if (array[i] < 0) {
+      result++;
     }
   }
-  return count;
+
+  return result;
 }
 
-console.log(findingBadData([-4, -9, -5, -33, -55]));
+console.log(findingBadData([1, -2, -5, -52]));
+
+/*
+The function starts by declaring a variable result and initializing it to 0. This variable will be used to store the count of negative numbers in the array.The function then uses a for loop to iterate over the elements in the array. Finally, the function returns the value of result, which is the number of negative numbers (Bad Data) found in the array.*/
 
 // Fifth Problem
 
-function zy(a, b, c) {
-  let first = a * 21;
-  let Second = b * 32;
-  let third = c * 43;
-  let sum = first + Second + third;
-  if (sum > 2000) {
+function gemsToDiamond(firstFriendGems, secondFriendGems, thirdFriendGems) {
+  if (isNaN(firstFriendGems, secondFriendGems, thirdFriendGems)) {
+    return "Please Enter all  valid number";
+  }
+  let sumFirstFriendGemsPower = firstFriendGems * 21;
+  let sumSecondFriendGemsPower = secondFriendGems * 32;
+  let sumThirdFriendGemsPower = thirdFriendGems * 43;
+  let sum =
+    sumFirstFriendGemsPower +
+    sumSecondFriendGemsPower +
+    sumThirdFriendGemsPower;
+  if (sum > 1000 * 2) {
     return sum - 2000;
   } else {
     return sum;
   }
 }
 
-console.log(zy(100, 5, 1));
+firstFriendGems = 100;
+secondFriendGems = 5;
+thirdFriendGems = 1;
+
+const diamond = gemsToDiamond(
+  firstFriendGems,
+  secondFriendGems,
+  thirdFriendGems
+);
+
+console.log(diamond);
+
+/*  
+the function calculates the sum of all three friends' gems power and stores it in the sum variable. If sum is greater than 2000, the function returns sum - 2000. Otherwise, it returns sum.
+*/
---
+++ test.js
@@ -0,0 +1,11 @@
+// function findingBadData(array) {
+//   let count = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] < 0) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// console.log(findingBadData([-4, -9, -5, -33, -55]));
